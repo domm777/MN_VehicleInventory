e322a94bd5b454ac8fba1841ff33cdc3e55125bf
domm777 <mnetsereab8473@conestogac.on.ca>
2026-02-17 19:36:47 -0500

    Part 1 DONE: Added 3 classes and methods to the 3 folders

---
+++ VehicleInventory.Domain/Exceptions/MN_DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory.Domain.Exceptions {
+    public class MN_DomainException : Exception {
+        public MN_DomainException(string message) : base(message) {}
+    }
+}
