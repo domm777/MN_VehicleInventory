3b3f6b85ef917a45a8972dc78eb56866a4ec4c0b
domm777 <mnetsereab8473@conestogac.on.ca>
2026-02-18 21:53:55 -0500

    Last Commit for Second submit fix

--- MN_VehicleInventory.WebAPI/Program.cs
+++ MN_VehicleInventory.WebAPI/Program.cs
@@ -4,24 +4,26 @@ using Microsoft.EntityFrameworkCore.SqlServer;
 using MN_VehicleInventory.Application.Interfaces;
 using MN_VehicleInventory.Application.Services;
 using MN_VehicleInventory.Infrastructure.Persistence;
-using MN_VehicleInventory.Infrastructure.Repositories; // Add the following using directive to enable UseSqlServer extension method
+using MN_VehicleInventory.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Registering the data base context with the dependency injection container
+// Add services to the container.
 builder.Services.AddDbContext<MN_InventoryDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register Repositories and Services (Dependency Injection)
+// Dependency Injection
 builder.Services.AddScoped<MN_IVehicleRepository, MN_VehicleRepository>();
 builder.Services.AddScoped<MN_IVehicleService, MN_VehicleService>();
 
+// Add controllers and Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline. We are in development environment, so we want to use Swagger for API documentation and testing.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
